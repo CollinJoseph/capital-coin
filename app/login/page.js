fe70e2c0a58a1b1bef2708fddbbe7f4ffa0b8f8c
Collin Joseph <collinjoseph00(@gmail.com>
2023-10-28 13:26:41 +0100

    login

---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <div>Login</div>
+      <div>username</div>
+      <div>login</div>
+    </div>
+  )
+}
